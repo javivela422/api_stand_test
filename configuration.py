7f8246182ac16fbf76b9a12de25b8c362e9ec352
francisco Javier Velasco Damián <131217568+javivela422@users.noreply.github.com>
2025-05-24 23:59:12 -0600

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-e7471686-9635-419f-acf8-6b2896fb990c.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
